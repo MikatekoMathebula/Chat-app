60fb05c91c01b46c91e975abe512f79699c07acc
MikatekoMathebula <ST10497919@rcconnect.edu.za>
2025-09-16 11:13:34 +0000

    completed Register Tests

--- test/chatapp/RegisterTest.java
+++ test/chatapp/RegisterTest.java
@@ -1,25 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package chatapp;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
  * @author RC_Student_lab
  */
 public class RegisterTest {
-
-    /**
-     * Test of checkUsername method, of class Register.
-     */
+    
     @Test
     public void usernameCorrectlyFormatted() {
         System.out.println("verifyUsername");
